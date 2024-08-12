eafc3d07505a771767eb66615d9e425d5add578f
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-08-12 22:07:12 +0800

    migrated original project files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,32 +6,59 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				headerShown: false,
+			}}>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon name="home" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="history"
+				options={{
+					title: 'History',
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon name="history" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="send"
+				options={{
+					title: 'Send',
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon name="paper-airplane" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="link"
+				options={{
+					title: 'Link',
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon name="credit-card" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="settings"
+				options={{
+					title: 'Settings',
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon name="gear" color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
 }
