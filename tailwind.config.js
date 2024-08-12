ddb7431d88632cfa6e7f1e883711b9b8fbf0a3e0
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-08-12 21:58:18 +0800

    nativewind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/tailwind/native")
+
+module.exports = {
+	content: ["./app/**.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [nativewind()],
+}
+
