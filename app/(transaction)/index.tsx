4327e5d48a6048a6ee7359586ec49d82722c8eec
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-08-28 00:33:00 +0800

    working room creation

--- app/(transaction)/index.tsx
+++ app/(transaction)/index.tsx
@@ -1,26 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { View, ScrollView, TouchableHighlight } from "react-native";
 import { Text, Avatar, Chip, IconButton, Card, Button } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { router } from "expo-router";
+import { router, useFocusEffect } from "expo-router";
+import { useIsFocused } from "@react-navigation/native";
 
-import { db, fs } from "@/firebase/config";
+import { db } from "@/firebase/config";
+import { ref, push, set, remove, onValue } from "firebase/database";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function TransactionHomeScreen() {
+    const [roomID, setRoomID] = useState("");
+
+    const isFocused = useIsFocused();
+    const wasFocused = useRef(false);
+
     const getInitials = (name: string) => {
-		const words = name.trim().split(" ");
-		let initials = "";
+        const words = name.trim().split(" ");
+        let initials = "";
 
-		for (let i = 0; i < Math.min(words.length, 2); i++) {
-			if (words[i].length > 0) {
-				initials += words[i][0].toUpperCase();
-			}
-		}
+        for (let i = 0; i < Math.min(words.length, 2); i++) {
+            if (words[i].length > 0) {
+                initials += words[i][0].toUpperCase();
+            }
+        }
 
-		return initials;
-	}
+        return initials;
+    }
 
+    const createRoom = async () => {
+        const uid = await AsyncStorage.getItem("UID");
+
+        if (uid) {
+            try {
+                const roomRef = push(ref(db, "rooms"));
+                const id = roomRef.key;
+
+                if (id) {
+                    setRoomID(id);
+
+                    await set(roomRef, {
+                        merchant: "",
+                        client: ""
+                    }).then(async () => {
+                        await set(ref(db, `rooms/${id}/users`), {
+                            uid: uid
+                        });
+                    });
+                }
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
+
+    const deleteRoom = async () => {
+        await remove(ref(db, `rooms/${roomID}`));
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+          let isScreenFocused = true;
+    
+          if (isScreenFocused) {
+            createRoom();
+          }
+    
+          return () => {
+            isScreenFocused = false; 
+            deleteRoom();
+          };
+        }, []) 
+      );
     return (
         <SafeAreaView className="flex flex-col w-screen h-screen gap-2 px-4 items-start justify-start">
             <Card className="w-full">
@@ -37,7 +91,7 @@ export default function TransactionHomeScreen() {
             <Card className="w-full">
                 <Card.Content className="flex flex-col gap-2">
                     <View>
-                        
+
                     </View>
                     <Button
                         icon={"qrcode-scan"}
