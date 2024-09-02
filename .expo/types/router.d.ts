5696e15ad7c2138ecedc8bbe3447e924c576aa5c
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-09-03 02:40:01 +0800

    created Lobby page

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -4,11 +4,11 @@ import * as Router from 'expo-router';
 export * from 'expo-router';
 
 declare module 'expo-router' {
-  export namespace ExpoRouter {
-    export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(login)` | `/(login)/` | `/(login)/registerIdentifier` | `/(login)/registerPass` | `/(tabs)` | `/(tabs)/` | `/(tabs)/history` | `/(tabs)/settings` | `/(transaction)` | `/(transaction)/` | `/(transaction)/scan` | `/(transaction)/lobby` | `/(transaction)/room` | `/_sitemap` | `/history` | `/registerIdentifier` | `/registerPass` | `/scan` | `/settings`;
-      DynamicRoutes: `/(tabs)/merchant/${Router.SingleRoutePart<T>}` | `/(tabs)/post/${Router.SingleRoutePart<T>}` | `/merchant/${Router.SingleRoutePart<T>}` | `/post/${Router.SingleRoutePart<T>}`;
-      DynamicRouteTemplate: `/(tabs)/merchant/[id]` | `/(tabs)/post/[id]` | `/merchant/[id]` | `/post/[id]`;
-    }
-  }
+	export namespace ExpoRouter {
+		export interface __routes<T extends string = string> extends Record<string, unknown> {
+			StaticRoutes: `/` | `/(login)` | `/(login)/` | `/(login)/registerIdentifier` | `/(login)/registerPass` | `/(tabs)` | `/(tabs)/` | `/(tabs)/history` | `/(tabs)/settings` | `/(transaction)` | `/(transaction)/` | `/(transaction)/lobby` | `/(transaction)/room` | `/(transaction)/scan` | `/_sitemap` | `/history` | `/lobby` | `/registerIdentifier` | `/registerPass` | `/room` | `/scan` | `/settings`;
+			DynamicRoutes: `/(tabs)/merchant/${Router.SingleRoutePart<T>}` | `/(tabs)/post/${Router.SingleRoutePart<T>}` | `/merchant/${Router.SingleRoutePart<T>}` | `/post/${Router.SingleRoutePart<T>}`;
+			DynamicRouteTemplate: `/(tabs)/merchant/[id]` | `/(tabs)/post/[id]` | `/merchant/[id]` | `/post/[id]`;
+		}
+	}
 }
--- app/(transaction)/lobby.tsx
+++ app/(transaction)/lobby.tsx
@@ -1,25 +1,80 @@
 import { useState, useEffect } from "react";
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, Avatar, Chip, IconButton, Card, Button } from "react-native-paper";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { db, fs } from "@/firebase/config";
+import { ref, push, set, remove, onValue } from "firebase/database";
+import { doc, getDoc, DocumentData } from "firebase/firestore";
+
+type MerchantData = {
+    id: string;
+    userName: string;
+    [key: string]: any;
+}
 
 export default function TransactionLobbyScreen() {
     const [roomID, setRoomID] = useState("");
     const [merchantID, setMerchantID] = useState("");
+    const [merchantData, setMerchantData] = useState<MerchantData>({ id: "", userName: "" });
+
+    const getMerchantID = async () => {
+        const merchantIDAsync = await AsyncStorage.getItem("merchantID");
+        if (merchantIDAsync) {
+            setMerchantID(merchantIDAsync);
+            console.log(merchantIDAsync);
+
+            getDoc(doc(fs, "Accounts", merchantIDAsync))
+                .then((sn) => {
+                    if (sn.data) {
+                        setMerchantData(curr => ({ ...curr, id: sn.id, ...sn.data() }));
+                    }
+                })
+        }
+    }
+
+    const getInitials = (name: string) => {
+        const words = name.trim().split(" ");
+        let initials = "";
+
+        for (let i = 0; i < Math.min(words.length, 2); i++) {
+            if (words[i].length > 0) {
+                initials += words[i][0].toUpperCase();
+            }
+        }
+
+        return initials;
+    }
 
     useEffect(() => {
-        (async () => {
-            const merchantIDAsync = await AsyncStorage.getItem("merchantID");
-            if (merchantIDAsync) {
-                setRoomID(merchantIDAsync);
-            }
-        });
+        getMerchantID();
     }, []);
 
-	return (
-		<SafeAreaView className="flex flex-col w-screen h-screen gap-2 p-2 items-start justify-start">
-			<View></View>
-		</SafeAreaView>
-	);
+    return (
+        <SafeAreaView className="flex flex-col w-screen h-screen p-2 items-start justify-start">
+            {merchantData.id ?
+                <View className="flex flex-col w-full h-full items-center justify-start">
+                    <Card className="w-full">
+                        <Card.Content className="flex flex-row w-full justify-between items-center">
+                            <View className="flex flex-row items-center gap-5">
+                                <Avatar.Text label={getInitials(merchantData.userName)} size={35} />
+                                <View className="flex">
+                                    <Text variant="titleLarge" className="font-bold">{merchantData.userName}</Text>
+                                    <Text variant="titleSmall" className="font-semibold text-ellipsis">{merchantData.id}</Text>
+                                </View>
+                            </View>
+                        </Card.Content>
+                    </Card>
+                    <Card>
+                        <Card.Content>
+                            <Text variant="bodyMedium">data to be placed here</Text>
+                        </Card.Content>
+                    </Card>
+
+                </View>
+                : null}
+        </SafeAreaView>
+    );
 }
