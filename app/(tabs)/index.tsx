9f020df5c1f1110ddc028b5034d926fd4cb3abec
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-09-21 01:40:17 +0800

    revamped Live Feed cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useTheme, Text, Card, Avatar, Chip, IconButton, FAB, List } from "react-native-paper";
+import { useTheme, Text, Card, Avatar, Chip, IconButton, FAB, Icon, Divider } from "react-native-paper";
 
 import { useIsFocused } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
@@ -12,11 +12,9 @@ import { supabase } from "@/supabase/config";
 
 import { useUserData } from "@/lib/context/UserContext";
 import { Transaction } from "@/lib/helpers/types";
-
-import { MaterialCommunityIcons as MCI } from "@expo/vector-icons";
+import { formatISODate } from "@/lib/helpers/functions";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { rgbaColor } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 type TabParamList = {
 	Home: undefined;
@@ -113,43 +111,48 @@ export default function HomeScreen({ navigation }: Props) {
 				: null}
 			<ScrollView className="w-full">
 				<View className="flex flex-col p-2 gap-4">
-					{transactions.map((trans: Transaction, i) => (
-						<Card 
-							key={i} 
-							onPress={() => router.navigate(`/transaction/${trans.id}`)}
-							style={{ backgroundColor: theme.colors.background }}
+					{transactions && transactions.map((transaction: Transaction, i) => (
+						<Card
+							key={i}
+							onPress={() => router.navigate(`/transaction/${transaction.id}`)}
+							style={{ 
+								backgroundColor: theme.colors.background,
+								borderColor: transaction.status === "completed" ? "#22c55e" : "#ef4444",
+							}}
+							className="border-b-8"
 						>
-							<Card.Content className="flex flex-col gap-2">
+							<Card.Content className="flex flex-col space-y-2">
 								<View className="flex flex-row w-full justify-between items-center">
-									<View className="flex flex-row items-center gap-5">
-										<Avatar.Text label={getInitials(JSON.parse(trans.merchant).username)} size={35} />
-										<View className="flex">
-											<Text variant="titleLarge" className="font-bold">{JSON.parse(trans.merchant).username}</Text>
-											<View className="flex flex-row items-center justify-start">
-												<MCI name="arrow-right-bottom" size={20} color={theme.colors.primary} />
-												<Text variant="titleMedium" className="font-semibold">{JSON.parse(trans.client).username}</Text>
-											</View>
-										</View>
-
-									</View>
-									{trans.status === "complete" ?
-										<MCI name="check-decagram" size={35} color={theme.colors.primary} />
+									<Text variant="titleSmall" className="text-slate-400">{formatISODate(transaction.created_at.toLocaleString())}</Text>
+									{transaction.status === "completed" ?
+										<Chip className="bg-green-200 text-green-200" compact={true}>
+											<Text variant="bodySmall">{transaction.status}</Text>
+										</Chip>
 										:
-										<MCI name="alert-decagram" size={35} color={theme.colors.primary} />
+										<Chip className="bg-red-200 text-red-200" compact={true}>
+											<Text variant="bodySmall">{transaction.status}</Text>
+										</Chip>
 									}
 								</View>
-								{/* <View className="flex flex-col items-start justify-center">
-									<Text variant="bodyMedium" className="font-bold">Total Amount Transacted</Text>
-									<Text variant="bodyLarge" className="font-semibold">{trans.total_amount}</Text>
-								</View>
-								<View className="flex flex-col items-start justify-center">
-									<Text variant="bodyMedium" className="font-bold">Platforms</Text>
-									<View className="flex flex-row items-center justify-start">
-										{trans.platforms.map((plt, i) => (
-											<Chip key={i}>{plt}</Chip>
-										))}
+								<View className="flex flex-col space-y-2">
+									<View className="flex flex-row space-x-2 items-center justify-start">
+										<Icon source="store" size={20} color={"#60a5fa"} />
+										<Avatar.Text label={getInitials(JSON.parse(transaction.merchant).username)} size={20} />
+										<Text variant="titleMedium" className="font-semibold">{JSON.parse(transaction.merchant).username}</Text>
 									</View>
-								</View> */}
+									<View className="flex flex-row space-x-2 items-center justify-start">
+										<Icon source="account" size={20} color={"#4ade80"} />
+										<Avatar.Text label={getInitials(JSON.parse(transaction.client).username)} size={20} />
+										<Text variant="titleMedium" className="font-semibold">{JSON.parse(transaction.client).username}</Text>
+									</View>
+								</View>
+								<View className="flex flex-row items-center justify-between">
+									<View className="flex flex-row space-x-2 items-center justify-start">
+										<Text variant="titleMedium" className="font-semibold text-slate-400">PHP</Text>
+										<Text variant="titleMedium" className="font-bold">{transaction.total_amount}</Text>
+									</View>
+									<Icon source="chevron-right" size={20} color={"#94a3b8"} />
+								</View>
 							</Card.Content>
 						</Card>
 					))}
