67a3fc18c8bb7cde719273a2c3404db36814e3ab
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-11-20 19:51:15 +0800

    added: working Privacy Policy in Registration

--- app/(login)/register.tsx
+++ app/(login)/register.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
-import { KeyboardAvoidingView, Platform, TouchableOpacity } from "react-native";
+import { KeyboardAvoidingView, Platform, ScrollView, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TextInput, Divider, ActivityIndicator } from "react-native-paper";
 
-import { View, Text, Button, Wizard, Colors } from "react-native-ui-lib";
+import { View, Text, Button, Wizard, Colors, Dialog, Checkbox, Modal } from "react-native-ui-lib";
 
 import { router } from "expo-router";
 
@@ -13,6 +13,7 @@ import { supabase } from "@/supabase/config";
 import { useUserData } from "@/lib/context/UserContext";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { dragDisable } from "d3";
 
 export default function RegisterScreen() {
     const [email, setEmail] = useState<string>("");
@@ -24,6 +25,9 @@ export default function RegisterScreen() {
 
     const [isRegistering, setIsRegistering] = useState<boolean>(false);
     const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false);
+
+    const [showPrivacyPolicy, setShowPrivacyPolicy] = useState<boolean>(false);
+    const [isPrivacyChecked, setIsPrivacyChecked] = useState<boolean>(false);
 
     const [activeIndex, setActiveIndex] = useState<number>(0);
     const [completedStepIndex, setCompletedStepIndex] = useState<number>(0);
@@ -202,12 +206,10 @@ export default function RegisterScreen() {
     }
 
     return (
-        <SafeAreaView 
-            className="flex flex-col w-screen h-screen px-4 pt-2 items-start justify-center"
+        <SafeAreaView
+            className="flex flex-col w-screen h-screen space-y-4 px-4 pt-2 items-start justify-center"
         >
             <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : "height"}
-                keyboardVerticalOffset={100}
                 className="flex w-full h-full space-y-4"
             >
                 <Text h2 className="font-bold mt-32">Create an Account</Text>
@@ -241,7 +243,7 @@ export default function RegisterScreen() {
                                     goToNextStep();
                                     break;
                                 case 1:
-                                    registerAccount();
+                                    setShowPrivacyPolicy(true);
                                     break;
                                 case 2:
                                     loginAccount();
@@ -314,6 +316,140 @@ export default function RegisterScreen() {
                         </Text>
                     </TouchableOpacity>
                 </View>
+                <Dialog
+                    visible={showPrivacyPolicy}
+                    onDismiss={() => setShowPrivacyPolicy(false)}
+                    // panDirection="up"
+                    containerStyle={{ backgroundColor: Colors.bgDefault, borderRadius: 8, padding: 4 }}
+                >
+                    <View
+                        className="flex flex-col w-full p-4 space-y-4"
+                    >
+                        <Text h3>Privacy Policy</Text>
+                        <ScrollView
+                            style={{ height: 300, width: "100%" }}
+                        >
+                            <Text caption>
+                                <Text caption className="font-bold">P2Trust Privacy Policy</Text>
+                                {"\n\n"}
+                                <Text caption className="font-bold">Effective Date:</Text> Nov 7, 2024{"\n\n"}
+                                P2Trust respects your privacy and is committed to protecting your personal information.
+                                This Privacy Policy explains how we collect, use, share, and protect the data you provide
+                                when using the P2Trust mobile application ("App"). By using the App, you agree to the
+                                terms outlined in this Privacy Policy.{"\n\n"}
+
+                                <Text caption className="font-bold">1. Information We Collect</Text>{"\n"}
+                                <Text caption className="font-bold">a. Personal Information</Text>{"\n"}
+                                i. We collect the following information for identification, account setup, and
+                                KYC (Know Your Customer) purposes:{"\n"}
+                                1. Full Name{"\n"}
+                                2. Email Address{"\n"}
+                                3. Mobile Number{"\n"}
+                                4. Identification (ID) Images: Collected during KYC verification for merchant
+                                assessment and fraud prevention.{"\n\n"}
+
+                                <Text caption className="font-bold">b. Transaction Data</Text>{"\n"}
+                                i. To enhance transaction transparency and security, P2Trust collects:{"\n\n"}
+                                <Text caption className="font-bold">c. Transaction History</Text>{"\n"}
+                                i. Transaction dates, amounts, and identifiers.{"\n\n"}
+                                <Text caption className="font-bold">d. Proof of Payment Images</Text>{"\n"}
+                                i. Uploaded by users to verify transactions.{"\n\n"}
+                                <Text caption className="font-bold">e. QR Codes</Text>{"\n"}
+                                i. Used to facilitate quick data entry and payment confirmation.{"\n\n"}
+
+                                <Text caption className="font-bold">2. Permissions We Request</Text>{"\n"}
+                                a. P2Trust requires specific permissions to enable core functionality, including camera
+                                access and file upload. Here’s how each permission is used:{"\n\n"}
+
+                                <Text caption className="font-bold">i. Camera Access</Text>{"\n"}
+                                1. Purpose: The camera is used to scan QR codes and capture images of IDs for KYC
+                                verification.{"\n"}
+                                2. Usage: Access is only active while scanning QR codes or capturing images for
+                                KYC and is not used for any other purpose. Images are not stored permanently unless
+                                uploaded by the user for verification purposes.{"\n\n"}
+
+                                <Text caption className="font-bold">ii. File Upload Access</Text>{"\n"}
+                                1. Purpose: File access allows users to upload images of QR codes and proofs of
+                                payment.{"\n"}
+                                2. Usage: Uploaded files are securely stored for transaction verification and fraud
+                                prevention purposes. Files can be deleted by users at any time, and we do not access
+                                files on your device outside of uploads initiated within the App.{"\n\n"}
+
+                                <Text caption className="font-bold">3. How We Use Your Information</Text>{"\n"}
+                                a. We use the information collected for the following purposes:{"\n"}
+                                i. User Verification: Personal and ID data verify account ownership and authenticity.{"\n"}
+                                ii. Transaction Security: Transaction history and proof of payments are used to confirm
+                                and assess transaction legitimacy.{"\n"}
+                                iii. Fraud Detection and Prevention: Transaction data and KYC images help protect users
+                                by identifying and flagging potential fraudulent activities.{"\n"}
+                                iv. Customer Support: Personal information helps us address inquiries and resolve
+                                issues.{"\n\n"}
+
+                                <Text caption className="font-bold">4. Data Sharing and Disclosure</Text>{"\n"}
+                                a. P2Trust will not sell, trade, or otherwise transfer your data to third parties without
+                                your consent, except:{"\n"}
+                                i. When Legally Required: We may disclose information to comply with legal obligations,
+                                such as court orders or regulatory requests.{"\n"}
+                                ii. For Security and Fraud Prevention: Data may be shared with trusted partners or law
+                                enforcement to detect and prevent fraudulent activities.{"\n\n"}
+
+                                <Text caption className="font-bold">5. Data Storage and Security</Text>{"\n"}
+                                a. We prioritize the security of your data. Measures include:{"\n"}
+                                i. Encryption: All sensitive data is encrypted during storage and transmission.{"\n"}
+                                ii. Access Control: Only authorized personnel have access to user data for legitimate
+                                purposes.{"\n"}
+                                b. We retain your data only as long as necessary for providing services, complying with
+                                legal requirements, or resolving disputes. Once no longer needed, data is securely
+                                deleted.{"\n\n"}
+
+                                <Text caption className="font-bold">6. User Rights and Control</Text>{"\n"}
+                                a. You have the following rights regarding your data:{"\n"}
+                                i. Access: Request a copy of the data we hold about you.{"\n"}
+                                ii. Correction: Update or correct inaccurate information.{"\n"}
+                                iii. Deletion: Request deletion of your account and personal information.{"\n"}
+                                iv. Withdrawal of Consent: Revoke consent for data usage by deleting your account.{"\n"}
+                                b. For any requests or inquiries, please contact us at
+                                <Text caption className="font-bold">contact.janscabs.dev@gmail.com</Text>.{"\n\n"}
+
+                                <Text caption className="font-bold">7. Changes to This Privacy Policy</Text>{"\n"}
+                                a. We may update this Privacy Policy to reflect changes in our practices or for legal
+                                compliance. Any updates will be posted in the App, and significant changes will prompt
+                                notification to all users. Your continued use of the App constitutes acceptance of the
+                                updated policy.{"\n\n"}
+
+                                <Text caption className="font-bold">8. Contact Us</Text>{"\n"}
+                                a. If you have questions regarding this Privacy Policy or P2Trust’s data practices,
+                                please contact us at:{" "}
+                                <Text caption className="font-bold">contact.janscabs.dev@gmail.com</Text>
+                            </Text>
+                        </ScrollView>
+                        <View className="flex flex-row w-full space-x-2 items-center">
+                            <Checkbox
+                                value={isPrivacyChecked}
+                                onValueChange={setIsPrivacyChecked}
+                            />
+                            <Text bodySmall>I understand P2Trust's Privacy Policy</Text>
+                        </View>
+                        <View className="flex flex-row w-full items-center justify-end space-x-2">
+                            <Button
+                                className="rounded-lg"
+                                style={{ backgroundColor: Colors.gray50 }}
+                                outline={true}
+                                outlineColor={Colors.gray900}
+                                onPress={() => setShowPrivacyPolicy(false)}
+                            >
+                                <Text buttonSmall gray900>Cancel</Text>
+                            </Button>
+                            <Button
+                                className="rounded-lg"
+                                onPress={() => registerAccount()}
+                                disabled={!isPrivacyChecked}
+                            >
+                                <Text buttonSmall white>Proceed</Text>
+                            </Button>
+                        </View>
+                    </View>
+                </Dialog>
             </KeyboardAvoidingView>
         </SafeAreaView>
     );
